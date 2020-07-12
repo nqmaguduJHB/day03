29152ea50f1b3426154e76f4886083d4dc427658
nqobile juwao magudu <nqmagudu@c5r7s5.wethinkcode.co.za>
2020-07-12 10:54:05 +0200

    Day03 exercise 07

---
+++ ex07/ft_strrev.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nqmagudu <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/07/12 10:23:31 by nqmagudu          #+#    #+#             */
+/*   Updated: 2020/07/12 10:51:19 by nqmagudu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <unistd.h>
+
+char	*ft_strrev(char *str)
+{
+	int index;
+	int a;
+	int b;
+	char temp;
+
+	length = 0;
+	while (str[index] != '\0')
+	{
+		index++;
+	}
+	a = 0;
+	b = index - 1;
+	while (a < b)
+	{
+		temp = str[a];
+		str[a] = str[b];
+		str[b] = temp;
+		a++;
+		b--;
+	}
+	return (str);
+}
+		
+
