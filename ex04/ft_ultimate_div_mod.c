aa2a561116da07f6f158df1d661acb49054dc901
nqmaguduJHB <67946111+nqmaguduJHB@users.noreply.github.com>
2020-07-13 08:34:25 +0200

    Update ft_ultimate_div_mod.c

--- ex04/ft_ultimate_div_mod.c
+++ ex04/ft_ultimate_div_mod.c
@@ -15,14 +15,15 @@
 
 void ft_ultimate_div_mod(int *a, int *b)
 {
-	int div;
-	int mod;
-
+	
 	if (*b != 0)
 	{
-		div = (*a) / (*b);
-		mod = (*a) % (*b);
-		*a = div;
-		*b = mod;
+		int temporal2;
+		int temporal2;
+		
+		temporal1 = *a / *b;
+		temporal2 = *a % *b;
+		*a = temporal1;
+		*b = temporal2;
 	}
 }
